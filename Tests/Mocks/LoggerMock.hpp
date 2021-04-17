280c64f14ca1838270ea532a43fec91f00a476a8
StEn <StEn@steven-england.info>
2021-04-17 09:09:34 +0200

    fix: Add prefix logging feature to mock

--- Tests/Mocks/LoggerMock.hpp
+++ Tests/Mocks/LoggerMock.hpp
@@ -9,12 +9,18 @@ namespace Logging
     class LoggerMock final : public ILogger
     {
     public:
-        void Fatal(const char* format, ...) override { printf(format); };
-        void Error(const char* format, ...) override { printf(format); };
-        void Warning(const char* format, ...) override { printf(format); };
-        void Notice(const char* format, ...) override { printf(format); };
-        void Trace(const char* format, ...) override { printf(format); };
-        void Verbose(const char* format, ...) override { printf(format); };
+        void Fatal(const char* format, ...) override { format; };
+        void Error(const char* format, ...) override { format; };
+        void Warning(const char* format, ...) override { format; };
+        void Notice(const char* format, ...) override { format; };
+        void Trace(const char* format, ...) override { format; };
+        void Verbose(const char* format, ...) override { format; };
+        void FatalP(const char* format, ...) override { format; };
+        void ErrorP(const char* format, ...) override { format; };
+        void WarningP(const char* format, ...) override { format; };
+        void NoticeP(const char* format, ...) override { format; };
+        void TraceP(const char* format, ...) override { format; };
+        void VerboseP(const char* format, ...) override { format; };
         MOCK_METHOD(void, Begin, (), (override));
     };
 }
