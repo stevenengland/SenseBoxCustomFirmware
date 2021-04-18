f773efff48b0925a7a8e13734b6a15592f6a7aaa
StEn <StEn@steven-england.info>
2021-04-18 08:59:15 +0200

    feat: Add NovaSds011 driver interface

---
+++ Components/Sensor/INovaSds011Driver.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace Sensor
+{
+    class INovaSds011Driver
+    {
+    public:
+        virtual ~INovaSds011Driver() = default;
+
+        virtual void Begin() = 0;
+        virtual void Read(float* p25, float* p10) = 0;
+    };
+}
