c049998093ad14c14067a929a261d6f7543c4cc7
StEn <StEn@steven-england.info>
2021-04-23 11:58:11 +0200

    feat: Make var a const char PROGMEM

--- Components/Network/examples/TcpStream/TcpStream.ino
+++ Components/Network/examples/TcpStream/TcpStream.ino
@@ -25,7 +25,7 @@ int Status = WL_IDLE_STATUS;
 // if you don't want to use DNS (and reduce your sketch size)
 // use the numeric IP instead of the name for the server:
 //IPAddress server(74,125,232,128);  // numeric IP for Google (no DNS)
-char HostName[] = "www.google.com";    // name address for Google (using DNS)
+const char HostName[] PROGMEM = "www.google.com";    // name address for Google (using DNS)
 
 // Initialize the Ethernet client library
 // with the IP address and port of the server
