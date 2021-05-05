2734c87119009aea683cb8c43fa709da40406a9e
StEn <StEn@steven-england.info>
2021-05-05 20:56:45 +0200

    feat: Add MeasurementUploaderMock

---
+++ Tests/Mocks/MeasurementUploaderMock.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <gmock.h>
+
+#include "../../Components/Measurement/IMeasurmentUploader.h"
+
+namespace Measurement
+{
+    class MeasurementUploaderMock final : public IMeasurementUploader
+    {
+    public:
+        MOCK_METHOD(TransmissionStatus, TrySendUpload, (IMeasurementContainer& container), (override));
+        MOCK_METHOD(TransmissionStatus, ReadUploadResponse, (char* responseBuffer, size_t lengthOfResponseBuffer, unsigned long timeout), (override));
+        MOCK_METHOD(void, EndUpload, (), (override));
+    };
+}
